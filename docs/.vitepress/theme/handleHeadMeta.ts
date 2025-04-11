c381fb56ff76cbdae943a91c0596c57a28d2c908
FunnyWolf <yu5890681@gmail.com>
2025-04-11 22:44:19 +0800

    update link

--- docs/.vitepress/theme/handleHeadMeta.ts
+++ docs/.vitepress/theme/handleHeadMeta.ts
@@ -12,29 +12,29 @@ export function handleHeadMeta(context: TransformContext) {
         : `${host}/viper-og.png`;
     const cardType = frontmatter.cover ? 'summary_large_image' : 'summary'
 
-    // Basic Meta tags
-    const basicTitle: HeadConfig = ["title", {}, title]
-    const basicDesc: HeadConfig = ["meta", {name: "description", content: curDesc}]
+    // 基础meta标签
+    const basicTitle: HeadConfig = ["meta", {property: "og:title", content: title}]
+    const basicDesc: HeadConfig = ["meta", {property: "og:description", content: curDesc}]
 
-    // Open Graph tags for Reddit and other platforms
-    const ogUrl: HeadConfig = ["meta", {property: "og:url", content: addBase(relativePath)}]
+    // Open Graph标签
+    const ogUrl: HeadConfig = ["meta", {property: "og:url", content: `${host}/${relativePath}`}]
     const ogTitle: HeadConfig = ["meta", {property: "og:title", content: title}]
     const ogDescription: HeadConfig = ["meta", {property: "og:description", content: curDesc}]
     const ogImage: HeadConfig = ["meta", {property: "og:image", content: cover}]
     const ogType: HeadConfig = ["meta", {property: "og:type", content: "article"}]
 
-    // Twitter Card tags
+    // Twitter Card标签
     const twitterCard: HeadConfig = ["meta", {name: "twitter:card", content: cardType}]
-    // const twitterTitle: HeadConfig = ["meta", {name: "twitter:title", content: title}]
-    // const twitterDescription: HeadConfig = ["meta", {name: "twitter:description", content: curDesc}]
-    // const twitterImage: HeadConfig = ["meta", {name: "twitter:image", content: cover}]
+    const twitterTitle: HeadConfig = ["meta", {name: "twitter:title", content: title}]
+    const twitterDescription: HeadConfig = ["meta", {name: "twitter:description", content: curDesc}]
+    const twitterImage: HeadConfig = ["meta", {name: "twitter:image", content: cover}]
     const twitterSite: HeadConfig = ["meta", {name: "twitter:site", content: "@viperrtp"}]
     const twitterCreator: HeadConfig = ["meta", {name: "twitter:creator", content: "@viperrtp"}]
 
     return [
         basicTitle, basicDesc,
-        ogUrl, ogTitle, ogDescription, ogImage, ogType, twitterCard,
-        // twitterTitle, twitterDescription, twitterImage,
+        ogUrl, ogTitle, ogDescription, ogImage, ogType,
+        twitterCard, twitterTitle, twitterDescription, twitterImage,
         twitterSite, twitterCreator
     ]
 }
